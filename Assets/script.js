ede14dff40b21e1015d91811f40308db054df76d
Mona Lisa <giatran1222@gmail.com>
2022-08-05 01:06:01 -0700

    timecode block

--- Assets/script.js
+++ Assets/script.js
@@ -7,24 +7,36 @@ var currentDay = $("#currentDay");
 function displayDate() {
     var today = moment().format("dddd, MMMM Do YYYY");
     currentDay.text(today);
+    console.log(today);
 }
 
 // function time-block to color code current hour, future hour and past hour
 function timeBlockColor() {
     var currentHour = moment().hour();
+    console.log(currentHour);
 
     $(".time-block").each(function () {
         var hour = parseInt($(this).attr("id"));
+        console.log(this);
 
         if (hour === currentHour) {
             $(this).addClass("present");
+            $(this).removeClass("future");
+            $(this).removeClass("past");
+
         } else if (hour > currentHour) {
             $(this).addClass("future");
+            $(this).removeClass("present");
+            $(this).removeClass("past");
         } else {
             $(this).addClass("past");
+            $(this).removeClass("present");
+            $(this).removeClass("future");
+
         }
     })
 }
+
 
 //local storage
 // function saveInput() {
@@ -50,24 +62,21 @@ function timeBlockColor() {
 // saveButton.addEventListener("click", function(event) {
 //     event.preventDefault(),
 
-//     // task = document.querySelector(".description");
+//     task = document.querySelector(".description");
 
 //     localStorage.setItem("task", description)
 // })
 
-function saveInput () {
-    var task = localStorage.getItem("task");
-    description.textContent = task;
-}
+// 
+// function saveInput () {
+//     var task = document.querySelector(".description").value;
+//     localStorage.setItem("task", task);
+// }
 
-saveButton.addEventListener("click", function(event){
-    event.preventDefault();
-
-    var task = document.querySelector(".description").value;
-
-    localStorage.setItem("task", task);
-    saveInput();
-})
+// saveButton.addEventListener("click", function(event){
+//     event.preventDefault();
+//     saveInput();
+// })
 
 displayDate();
 timeBlockColor()
