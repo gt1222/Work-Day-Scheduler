b6173effc0e58e51fed0121d6b47a70a4092dd0a
Mona Lisa <giatran1222@gmail.com>
2022-08-04 20:55:41 -0700

    timeblock color maybe, current day, some save btn

--- Assets/script.js
+++ Assets/script.js
@@ -1,11 +1,39 @@
- var currentDayEl = $("#currentDay");
+var saveButton = document.querySelector(".saveBtn");
 
+//displaying the current date
+var currentDay = $("#currentDay");
 
- //displaying the current date
 function displayDate() {
     var today = moment().format("dddd, MMMM Do YYYY");
-    currentDayEl.text(today);
+    currentDay.text(today);
 }
-displayDate();
 
- 
+// function time-block to color code current hour, future hour and past hour
+function timeBlockColor() {
+    var hour = moment().hour();
+
+    $(".time-block").each(function () {
+        var currentHour = parseInt($(this).attr("id"));
+
+        if (currentHour === hour) {
+            $(this).addClass("present");
+        } else if (currentHour > hour) {
+            $(this).addClass("future");
+        } else {
+            $(this).addClass("past");
+        }
+    })
+}
+
+//local storage
+// saveButton.addEventListener("click", function (event) {
+//     event.preventDefault();
+
+//     var task = 
+
+
+// };
+
+displayDate();
+timeBlockColor()
+
